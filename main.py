bb71f3c95d9037e157dd8ac80e70b4d650a46527
Najib Adan <najibadan@airmail.cc>
2021-09-12 09:13:01 +0300

    Implemented an args parsers that takes in the width, height of the board and the number of bombs in the board

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from random import randint
-from Cell import Cell, Bomb
-NUMBER_OF_BOMBS = 40
-positionOfBombs = Bomb.initializeBombs(NUMBER_OF_BOMBS)
+from Cell import Cell, Board
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("width")
+ap.add_argument("height")
+ap.add_argument("bombs")
+args = vars(ap.parse_args())
+board = Board(args['width'],args['height'],args['bombs'])
+positionOfBombs = board.initializeBombs()
 cells = []
 for bomb in positionOfBombs:
-    for cell in bomb.getNeighbors():
+    for cell in bomb.getNeighbors(board):
         if cell not in cells:
             cell.increaseBomb()
             cells.append(cell)
         else:
             cells[cells.index(cell)].increaseBomb()
-        
 
-for i in range(1,17):
-    for j in range(1,17):
+for i in range(1,board.width+1):
+    for j in range(1,board.height+1):
         dummyCell = Cell(i,j)
         if dummyCell in cells:
             print(cells[cells.index(dummyCell)].numberOfBombs,end="")
-        elif Cell(i,j) in positionOfBombs:
+        elif Cell(i,j,True) in positionOfBombs:
             print('*',end='')
         else:
             print(' ',end='')
     print()
-
-
